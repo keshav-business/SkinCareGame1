f0e283200537f0affb297dd15e98bdb4dbcb9d18
Keshav Madan <157604059+keshav-business@users.noreply.github.com>
2025-07-02 13:49:18 +0530

    Add files via upload

--- scripts/c3runtime.js
+++ scripts/c3runtime.js
@@ -1500,12 +1500,8 @@ self.C3_ExpressionFuncs = [
 			const n1 = p._GetNode(1);
 			return () => f0(n1.ExpObject(), "[^0-9]", "", "");
 		},
-		p => {
-			const f0 = p._GetNode(0).GetBoundMethod();
-			const n1 = p._GetNode(1);
-			return () => f0(n1.ExpObject(), "@");
-		},
-		() => -1,
+		() => "^[\\w.-]+@[\\w.-]+\\.\\w+$",
+		() => "i",
 		() => "BEGIN GAME",
 		() => -10,
 		() => 6,
